06ff1c9cb9c03501819d72660191d6944d67e806
KrishnapriyaA4320 <84850151+KrishnapriyaA4320@users.noreply.github.com>
2021-06-06 19:25:56 +0530

    Project Files

---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        This was an amazing 7days bootcamp hosted by ShapeAI and taken up by
+        Mr.Shaurya Sinha.We covered everything from scratch including
+        HTML,Javascript and React.js.We really enjoyed the classes and each
+        session was interesting.
+      </p>
+    </div>
+  );
+}
+export default Note;
